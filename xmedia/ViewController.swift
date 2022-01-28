2d1f67a25413661765b8137084639f5bba84187b
Stefan Jivalino <stefanjivalino3@gmail.com>
2022-01-28 16:48:20 +0700

    Initial Commit

---
+++ xmedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xmedia
+//
+//  Created by Personal on 28/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
